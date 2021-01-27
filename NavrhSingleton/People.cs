e081dfffc1825648042bb0498b260b62f0afdff5
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2021-01-27 15:07:41 +0100

    Add project files.

---
+++ NavrhSingleton/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavrhSingleton
+{
+    public class People
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public int RodneCislo { get; set; }
+        public People(string jmeno, string prijmeni, DateTime datumNarozeni, int rodneCislo)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            DatumNarozeni = datumNarozeni;
+            RodneCislo = rodneCislo;
+        }
+    }
+}
